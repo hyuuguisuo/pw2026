86b76d37d8a7f18e3e232d20abb483ec942a61f0
hyuuguisuo <guihnq@gmail.com>
2026-03-12 09:11:39 -0300

    salv

--- locadora/urls.py
+++ locadora/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
   path("inicio/", Index.as_view(), name="página_inicial"),
   path("sobre/", Sobre.as_view(), name="sobre"),
-   path("contato/", Contato.as_view(), name="contato")
+  path("contato/", Contato.as_view(), name="contato")
 ]
