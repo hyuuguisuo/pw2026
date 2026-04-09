09a1a4804a97b1f0fd8ea1967313e611315675b1
hyuuguisuoo <guihnq@gmail.com>
2026-04-09 09:39:14 -0300

    nico

--- locadora/urls.py
+++ locadora/urls.py
@@ -4,5 +4,12 @@ from .views import *
 urlpatterns = [
   path("inicio/", Index.as_view(), name="página_inicial"),
   path("sobre/", Sobre.as_view(), name="sobre"),
-  path("contato/", Contato.as_view(), name="contato")
+  path("contato/", Contato.as_view(), name="contato"),
+  
+  # ## URL FORNECEDOR
+  path("cadastrar/fornecedor/", Create_Fornecedor.as_view(), name="novo_fornecedor"),
+  path("listar/fornecedor/", List_Fornecedor.as_view(), name="listar_fornecedor"),
+  path("excluir/fornecedor/<int:pk>/", Delete_Fornecedor.as_view(), name="excluir_fornecedor"),
+  path("editar/fornecedor/<int:pk>/", Update_Fornecedor.as_view(), name="editar_fornecedor"),
+  path("detalhes/fornecedor/<int:pk>/", Detail_Fornecedor.as_view(), name="detalhes_fornecedor"),
 ]
--- locadora/views.py
+++ locadora/views.py
@@ -1,5 +1,15 @@
 from django.views.generic import TemplateView
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+
+from django.urls import reverse_lazy
+
+from .models import Cliente, Fornecedor, Genero, Categoria
+
+
 class Index(TemplateView):
     template_name = "locadora/index.html"
 
@@ -8,3 +18,96 @@ class Sobre(TemplateView):
 
 class Contato(TemplateView):
     template_name = "locadora/contato.html"
+    
+    
+    
+#####  VIEWS FORNECEDOR 
+
+
+class Create_Fornecedor(CreateView):
+    model = Fornecedor
+    fields = ['nome', 'cnpj', 'contato', 'endereco', 'tipo_distribuicao']
+    template_name = 'locadora/form.html'
+    success_url = reverse_lazy('página_inicial')
+    extra_context = {
+        "titulo" : "Cadastro de Fornecedor",
+        "botao" : "Cadastrar"
+    }
+
+
+class Update_Fornecedor(UpdateView):
+    model = Fornecedor
+    fields = ['nome', 'email', 'cpf', 'telefone', 'codigo']
+    template_name = 'locadora/form.html'
+    success_url = reverse_lazy('página_inicial')
+    extra_context = {
+        "titulo" : "Edição de Fornecedor",
+        "botao" : "Salvar"
+    }
+    
+    
+class Delete_Fornecedor(DeleteView):
+    model = Fornecedor
+    template_name = 'locadora/form.html'
+    success_url = reverse_lazy('página_inicial')
+    extra_context = {
+        "titulo": "Excluir Fornecedor",
+        "botao": "OBLITERAR"
+    }
+    
+    
+class List_Fornecedor(ListView):
+    model = Fornecedor
+    template_name = 'locadora/form.html'
+    
+    
+class Detail_Fornecedor(DetailView):
+    model = Fornecedor
+    template_name = 'locadora/form.html'
+
+
+
+# VIEWS CLIENT
+
+
+class Create_Cliente(CreateView):
+    model = Cliente
+    fields = ['nome', 'email', 'cpf', 'telefone', 'codigo']
+    template_name = 'locadora/form.html'
+    success_url = reverse_lazy('página_inicial')
+    extra_context = {
+        "titulo": "Cadastro de Cliente",
+        "botao": "Cadastrar"
+    }
+
+
+class Update_Cliente(UpdateView):
+    model = Cliente
+    fields = ['nome', 'email', 'cpf', 'telefone', 'codigo']
+    template_name = 'locadora/form.html'
+    success_url = reverse_lazy('página_inicial')
+    extra_context = {
+        "titulo": "Edição de Cliente",
+        "botao": "Salvar"
+    }
+
+
+class Delete_Cliente(DeleteView):
+    model = Cliente
+    template_name = 'locadora/form.html'
+    success_url = reverse_lazy('página_inicial')
+    extra_context = {
+        "titulo": "Excluir Cliente",
+        "botao": "OBLITERAR"
+    }
+
+
+class List_Cliente(ListView):
+    model = Cliente
+    template_name = 'locadora/form.html'
+
+
+class Detail_Cliente(DetailView):
+    model = Cliente
+    template_name = 'locadora/form.html'
+
